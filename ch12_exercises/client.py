e45ac64f6a4e1612221bbd8f5a621523a623df22
Ashley Sands <ashley.j.sands@gmail.com>
2017-10-07 15:27:23 +1000

    Add support for resource containers.

--- ch12_exercises/client.py
+++ ch12_exercises/client.py
@@ -3,9 +3,12 @@ import tensorflow as tf
 
 parser = argparse.ArgumentParser(description="a tensorflow MNIST client.")
 parser.add_argument('server_host', type=str, help='the server host name and port')
+parser.add_argument('container_name', type=str, help='the name of the resource container to use', nargs='?', default='')
 args = parser.parse_args()
 
-c = tf.constant("hello world")
+with tf.container(args.container_name):
+	c = tf.constant("hello world")
 
+print("Connecting to %s to use '%s' container" % (args.server_host, args.container_name))
 with tf.Session("grpc://" + args.server_host) as sess:
 	print(c.eval())
